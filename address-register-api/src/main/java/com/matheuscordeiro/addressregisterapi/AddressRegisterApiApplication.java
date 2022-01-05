b17473b28f41de502c2c1365b540d42e7ff99fc2
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-01-04 22:44:35 -0300

    Create project

---
+++ address-register-api/src/main/java/com/matheuscordeiro/addressregisterapi/AddressRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.addressregisterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressRegisterApiApplication.class, args);
+	}
+
+}
